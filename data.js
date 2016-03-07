cdf4f128f7289ff7fa1eea47a493557569998c26
Christopher Mehmed <chrismehmed@gmail.com>
2016-03-07 18:45:57 -0500

    post route handler

--- data.js
+++ data.js
@@ -1,17 +1,7 @@
 module.exports = [
 	{
-		id: 1,
-		description: 'Get game',
-		completed: false
-	},
-	{
-		id: 2,
-		description: 'reset bookshelf',
-		completed: false
-	},
-	{
 		id: 3,
-		description: 'postman naming convention',
+		description: 'Check the postman GET requests',
 		completed: true
 	}
 ];
--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var port = process.env.PORT || 3000;
 var dbCollection = require('./data.js');
+var idCounter = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
 	res.send('sup now');
+});
+
+app.post('/minders', function (req, res) {
+	var body = req.body;
+	body.id = idCounter++;
+	dbCollection.push(body);
+	res.json(body);
 });
 
 app.get('/minders', function (req, res) {
