3d9417d1b90491035aa23cde874f7f677e68a890
Christopher Mehmed <chrismehmed@gmail.com>
2016-03-06 21:41:54 -0500

    mock up db in a JS file

---
+++ data.js
@@ -0,0 +1,17 @@
+module.exports = [
+	{
+		id: 1,
+		description: 'Get game',
+		completed: false
+	},
+	{
+		id: 2,
+		description: 'reset bookshelf',
+		completed: false
+	},
+	{
+		id: 3,
+		description: 'postman naming convention',
+		completed: true
+	}
+];
--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
+var dbCollection = require('./data.js');
 
 app.get('/', function (req, res) {
 	res.send('sup now');
+});
+
+app.get('/minders', function (req, res) {
+	res.json(dbCollection);
+});
+
+app.get('/minders/:id', function (req, res) {
+	var id = parseInt(req.params.id, 10);
+	var matchingObj;
+
+	for ( item in dbCollection ) {
+		if ( dbCollection[item].id === id ) {
+			matchingObj = dbCollection[item];
+		}
+	}
+
+	if ( matchingObj ) {
+		res.json(matchingObj);
+	} else {
+		res.status(404).send();
+	}
 });
 
 app.listen(port, function () {
