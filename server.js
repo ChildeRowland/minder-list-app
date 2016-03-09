9121e2dffbab9a27e919ecc1ebba52f7de8065af
Christopher Mehmed <chrismehmed@gmail.com>
2016-03-09 15:05:34 -0500

    demo db with sequelize

--- server.js
+++ server.js
@@ -9,44 +9,52 @@ var idCounter = 2;
 
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
 	res.send('sup now');
 });
 
-app.get('/minders', function (req, res) {
+app.get('/minders', function(req, res) {
 	var queryParams = req.query;
 	var filteredMinders = dbCollection;
 
-	if ( queryParams.completed === 'false' ) {
-		filteredMinders = _.where(filteredMinders, {completed: false});
-	} else if ( queryParams.completed === 'true' ) {
-		filteredMinders = _.where(filteredMinders, {completed: true});
+	if (queryParams.completed === 'false') {
+		filteredMinders = _.where(filteredMinders, {
+			completed: false
+		});
+	} else if (queryParams.completed === 'true') {
+		filteredMinders = _.where(filteredMinders, {
+			completed: true
+		});
 	}
 
-	if ( queryParams.q && queryParams.q.length > 0 ) {
+	if (queryParams.q && queryParams.q.length > 0) {
 		filteredMinders = _.filter(filteredMinders, function(minder) {
 			return minder.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
 		});
 	}
-	
+
 	res.json(filteredMinders);
 });
 
-app.get('/minders/:id', function (req, res) {
+app.get('/minders/:id', function(req, res) {
 	var minderId = parseInt(req.params.id, 10);
-	var matchingObj = _.findWhere(dbCollection, {id: minderId});
+	var matchingObj = _.findWhere(dbCollection, {
+		id: minderId
+	});
 
-	if ( matchingObj ) {
+	if (matchingObj) {
 		res.json(matchingObj);
 	} else {
-		res.status(404).json({"error": "There is no matching entry"});
+		res.status(404).json({
+			"error": "There is no matching entry"
+		});
 	}
 });
 
-app.post('/minders', function (req, res) {
+app.post('/minders', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
-	if ( !_.isString(body.description) || body.description.trim().length === 0 || !_.isBoolean(body.completed) ) {
+	if (!_.isString(body.description) || body.description.trim().length === 0 || !_.isBoolean(body.completed)) {
 		return res.status(400).send();
 	}
 
@@ -57,49 +65,56 @@ app.post('/minders', function (req, res) {
 	res.json(body);
 });
 
-app.put('/minders/:id', function (req, res) {
+app.put('/minders/:id', function(req, res) {
 	var minderId = parseInt(req.params.id, 10);
-	var matchingObj = _.findWhere(dbCollection, {id: minderId});
+	var matchingObj = _.findWhere(dbCollection, {
+		id: minderId
+	});
 	var body = _.pick(req.body, 'description', 'completed');
 	var validAttributes = {};
 
-	if ( !matchingObj ) {
-		return res.status(404).json({"error": "Entry not found"});
+	if (!matchingObj) {
+		return res.status(404).json({
+			"error": "Entry not found"
+		});
 	}
 
-	if ( body.hasOwnProperty('completed') && _.isBoolean(body.completed) ) {
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
 		validAttributes.completed = body.completed;
-	} else if ( body.hasOwnProperty('completed') ) {
-		return res.status(400).json({"error": "Not a valid change for 'completed'."})
-	} 
+	} else if (body.hasOwnProperty('completed')) {
+		return res.status(400).json({
+			"error": "Not a valid change for 'completed'."
+		})
+	}
 
-	if ( body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0 ) {
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
 		validAttributes.description = body.description;
-	} else if ( body.hasOwnProperty('description') ) {
-		return res.status(400).json({"error": "Not a valid change for 'description'."});
+	} else if (body.hasOwnProperty('description')) {
+		return res.status(400).json({
+			"error": "Not a valid change for 'description'."
+		});
 	}
 
 	_.extend(matchingObj, validAttributes);
 	res.json(matchingObj);
 });
 
-app.delete('/minders/:id', function (req, res) {
+app.delete('/minders/:id', function(req, res) {
 	var minderId = parseInt(req.params.id, 10);
-	var matchingObj = _.findWhere(dbCollection, {id: minderId});
+	var matchingObj = _.findWhere(dbCollection, {
+		id: minderId
+	});
 
-	if ( matchingObj ) {
+	if (matchingObj) {
 		dbCollection = _.without(dbCollection, matchingObj);
 		res.json(matchingObj);
 	} else {
-		res.status(404).json({"error": "There is no matching entry"});
+		res.status(404).json({
+			"error": "There is no matching entry"
+		});
 	}
 });
 
-app.listen(port, function () {
+app.listen(port, function() {
 	console.log('Express is listening on ' + port);
 });
-
-
-
-
-
